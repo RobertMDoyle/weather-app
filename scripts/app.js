fb651df9263db1c5e4bb24217d0b1006c5b06ae7
Rob Doyle <robdoyle@ymail.com>
2019-09-19 20:41:36 +0100

    original version complete

--- scripts/app.js
+++ scripts/app.js
@@ -59,7 +59,17 @@ cityForm.addEventListener('submit', e => {
     updateCity(city)
     .then(data => updateUI(data))
     .catch(err => console.log(err));
+
+
+    // Set Local Storage
+    localStorage.setItem('city', city);
 });
+
+if (localStorage.getItem('city')) {
+    updateCity(localStorage.getItem('city'))
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+} 
 
 
 const result = true ? 'value 1' : 'value 2';
